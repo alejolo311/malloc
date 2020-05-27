b1c34b4fe991622a54948d3e0a16cec59d562f24
alejolo311 <alejolo311@gmail.com>
2020-05-27 18:08:10 -0500

    fix style

--- naive_malloc.c
+++ naive_malloc.c
@@ -1,10 +1,15 @@
 #include "malloc.h"
-
+/**
+ * naive_malloc - Program entry point
+ * @size: the size
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
 void *naive_malloc(size_t size)
 {
-  void *begin;
-  begin = sbrk(0);
-  brk(begin);
-  begin = sbrk(size);
-  return sbrk(0);
+	void *begin;
+
+	begin = sbrk(0);
+	brk(begin);
+	begin = sbrk(size);
+	return (sbrk(0));
 }
